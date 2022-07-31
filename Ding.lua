cb74ef9be6383c3a8de8dd0b02f226f0ab94bc29
Durian-Life <66036934+Durian-Life@users.noreply.github.com>
2022-08-01 01:00:07 +0800

    Update Ding.lua

--- Ding.lua
+++ Ding.lua
@@ -45,7 +45,7 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long.dingtalk.com:80 HTTP/1.1\r\n' ..
-                    'Host:down.dingtalk.com:80\r\n' ..
+                    'Host:tms.dingtalk.com:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'User-Agent:baiduboxapp\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
