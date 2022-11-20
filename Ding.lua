722429927ebec327cb166a990fcd31194340c0c4
Durian-Life <66036934+Durian-Life@users.noreply.github.com>
2022-11-21 01:03:23 +0800

    Update Ding.lua

--- Ding.lua
+++ Ding.lua
@@ -1,7 +1,8 @@
--- file: lua/backend-baidu.lua
+-- file: lua/Halt.lua
 
 local http = require 'http'
 local backend = require 'backend'
+
 
 local char = string.char
 local byte = string.byte
@@ -26,30 +27,53 @@ local ctx_write = backend.write
 local ctx_free = backend.free
 local ctx_debug = backend.debug
 
+local is_http_request = http.is_http_request
+
 local flags = {}
+local marks = {}
 local kHttpHeaderSent = 1
 local kHttpHeaderRecived = 2
 
 function wa_lua_on_flags_cb(ctx)
-    return DIRECT_WRITE
+    return 0
 end
 
 function wa_lua_on_handshake_cb(ctx)
     local uuid = ctx_uuid(ctx)
 
+    if ( ctx_address_port(ctx) == 8080 ) then
+        flags[uuid] = kHttpHeaderRecived
+    end
+
+    -- local proxy_type = ctx_proxy_type(ctx)
+
+    -- if proxy_type == PROXY.HTTP_TUNNEL then
+    --     ctx_debug('proxy_type -- ' .. ctx_address_host(ctx) .. ' -- ' .. 'PROXY.HTTP_TUNNEL')
+    -- elseif proxy_type == PROXY.SOCKS_TUNNEL then
+    --     ctx_debug('proxy_type -- ' .. ctx_address_host(ctx) .. ' -- ' .. 'PROXY.SOCKS_TUNNEL')
+    -- else
+    --     ctx_debug('proxy_type -- ' .. ctx_address_host(ctx) .. ' -- ' .. 'none')
+    -- end
+
+
     if flags[uuid] == kHttpHeaderRecived then
         return true
     end
+    
+    local res = nil
+    
 
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long.dingtalk.com:80 HTTP/1.1\r\n' ..
-                    'Host:tms.dingtalk.com:80\r\n' ..
+        
+
+        res = 'CONNECT ' .. host .. ':' .. port ..'@listen.10155.com HTTP/1.1\r\n' ..
+                    'Host: listen.10155.com\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'User-Agent:baiduboxapp\r\n'..
-                    'Proxy-Connection: Keep-Alive\r\n'..
-                    'X-T5-Auth: YTY0Nzlk\r\n\r\n'
+                    'X-T5-Auth: YTY0Nzlk\r\n' ..
+                    'User-Agent: baiduboxapp\r\n\r\n'
+          
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
     end
@@ -58,22 +82,53 @@ function wa_lua_on_handshake_cb(ctx)
 end
 
 function wa_lua_on_read_cb(ctx, buf)
-    ctx_debug('wa_lua_on_read_cb')
+
     local uuid = ctx_uuid(ctx)
     if flags[uuid] == kHttpHeaderSent then
         flags[uuid] = kHttpHeaderRecived
         return HANDSHAKE, nil
     end
+
     return DIRECT, buf
 end
 
 function wa_lua_on_write_cb(ctx, buf)
-    ctx_debug('wa_lua_on_write_cb')
+ 
+    local host = ctx_address_host(ctx)
+    local port = ctx_address_port(ctx)
+
+    -- ctx_debug('buffer -- ' .. 'before' .. ' -- ' .. buf)
+    
+    if ( is_http_request(buf) == 1 ) then
+            local index, tmp = find(buf, '/')
+            local method = sub(buf, 1, index - 2)
+            -- local rest = sub(buf, index)
+            -- local s, e = find(rest, '\r\n')
+
+            -- local rest = sub(buf, index)
+            local s, e = find(buf, '\r\n')
+            
+            -- local less = sub(rest, e + 1)
+            -- local s1, e1 = find(less, '\r\n')
+
+            -- buf = sub(buf, 1, e) .. 
+            buf = method .. ' http://' .. host .. ':' .. port .. sub(buf, index, e) ..
+            '@listen.10155.com\r\n'..
+            'X-T5-Auth: YTY0Nzlk\r\n' ..
+            'User-Agent: baiduboxapp\r\n' ..
+            sub(buf, e + 1)
+
+            -- ctx_debug('buffer -- ' .. host .. ' -- ' .. buf)
+
+            ctx_write(ctx, buf)
+            return SUCCESS, nil
+            
+    end
+    
     return DIRECT, buf
 end
 
 function wa_lua_on_close_cb(ctx)
-    ctx_debug('wa_lua_on_close_cb')
     local uuid = ctx_uuid(ctx)
     flags[uuid] = nil
     ctx_free(ctx)
